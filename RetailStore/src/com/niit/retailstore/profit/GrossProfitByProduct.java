f6a4237989109fc00569412a9aabd0f63112dfef
hduser <hduser@bhayyasaheb>
2017-11-10 02:04:54 +0530

    Find total gross profit made by each product and also by each category for all the 4 months data. Completed

---
+++ RetailStore/src/com/niit/retailstore/profit/GrossProfitByProduct.java
@@ -0,0 +1,77 @@
+package com.niit.retailstore.profit;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class GrossProfitByProduct {
+
+	
+	public static class ProfitMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+	{
+
+		@Override
+		protected void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException 
+		{
+			String[] part = value.toString().split(";");
+			
+			String productId = part[5];
+			
+			int totalCost = Integer.parseInt(part[7]);
+			int totalSales = Integer.parseInt(part[8]);
+			
+			int profit = totalSales - totalCost;
+			
+			context.write(new Text(productId), new IntWritable(profit));
+		}
+		
+	}
+	
+	
+	public static class ProfitReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		
+		@Override
+		protected void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException 
+		{
+			int sum =0;
+			
+			for(IntWritable val : values)
+			{
+				sum += val.get();
+			}
+			
+			context.write(new Text(key), new IntWritable(sum));
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf,"Gross Profit By Product Id");
+		
+		job.setJarByClass(GrossProfitByProduct.class);
+		job.setMapperClass(ProfitMapper.class);
+		job.setReducerClass(ProfitReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+	}
+
+}
