ae2edaac7afc7cb84744642bda3b16a3908cc16d
bhayyasaheb <bhayyasahebkoke@gmail.com>
2017-12-07 00:07:44 +0530

    Created UDF Project in that UnixtimeToDate class in that we convert milliseconds time to Date using hive-exec.jar, commons-logging.jar and hadoop-common.jar

---
+++ UDF/src/com/niit/udfhive/UnixtimeToDate.java
@@ -0,0 +1,24 @@
+package com.niit.udfhive;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class UnixtimeToDate extends UDF{
+
+	public Text evaluate(Text text)
+	{
+		if(text == null) return null;
+		long timestamp = Long.parseLong(text.toString());
+		return new Text(toDate(timestamp));
+	}
+	
+	private String toDate(long timestamp)
+	{
+		Date date = new Date(timestamp);
+		return DateFormat.getInstance().format(date).toString();
+	}
+
+}
